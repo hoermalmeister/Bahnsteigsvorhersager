959a1b2bc607e3eca40894080605fe14d0251503
Petr Novák <petr.novak.gogo@gmail.com>
2026-08-18 17:41:02 +0200

    Update postprocessor.py

--- postprocessor.py
+++ postprocessor.py
@@ -28,7 +28,7 @@ def run_postprocessing():
     ''')
     
     # Určíme včerejší datum (abychom zpracovávali jen vlaky, které už jistě dojely)
-    yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+    yesterday = (datetime.now() - timedelta(days=0)).strftime('%Y-%m-%d') //má být jednička
     print(f"Spouštím post-processing pro datum: {yesterday}")
 
     # 2. Načtení všech včerejších spojů ze surové tabulky
